8ce7ef4f71edc8139f8c7d7d0409af35f37e595e
b1shal711 <ba070101@gmail.com>
2023-01-21 20:33:39 +0530

    change

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ passport.deserializeUser(function(obj, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://dark-rose-pike-slip.cyclic.app/auth/google/course"
+    callbackURL: "/auth/google/course"
   },
   function(accessToken, refreshToken, profile, done) {
     userProfile = profile;
@@ -73,7 +73,7 @@ function(req, res) {
 passport.use(new FacebookStrategy({
     clientID: process.env.APP_ID,
     clientSecret: process.env.APP_SECRET,
-    callbackURL: "https://dark-rose-pike-slip.cyclic.app/auth/facebook/course"
+    callbackURL: "/auth/facebook/course"
   },
   function(accessToken, refreshToken, profile, done) {
     userProfile = profile;
